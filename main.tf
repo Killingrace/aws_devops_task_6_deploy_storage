63fb44fb1003486137860a247dc5fd641e4b566a
Killingrace <dima.shutko5@gmail.com>
2026-07-19 23:36:52 +0200

    ALL

--- main.tf
+++ main.tf
@@ -1,3 +1,47 @@
-# 1. Deploy an S3 storage bucket 
+resource "aws_s3_bucket" "this" {
+  bucket = "my-tf-test-bucket"
 
-# 2. Confugure bucket policy to allow grafana iam role to use storage 
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-grafana_iam_role_arn = ""
+grafana_iam_role_arn = "arn:aws:iam::191913451155:role/GrafanaRole"
